f4751d28ff880333453a555c38b0d6102ccd2b8b
NghiLam1106 <lam91391@gmail.com>
2024-11-28 16:37:29 +0700

    first commit

---
+++ src/test/java/com/ca/webphim/WebphimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ca.webphim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebphimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
